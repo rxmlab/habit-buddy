bca8dc37cec15dd345abd3b35ee8fcaf4e5c257e
ram <ramjawade.dev@gmail.com>
2026-05-17 19:33:02 +0530

    admin fixed

--- projects/habit-buddy/src/app/shared/components/bottom-nav/bottom-nav.component.ts
+++ projects/habit-buddy/src/app/shared/components/bottom-nav/bottom-nav.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { LucideAngularModule, Home, Calendar, BarChart3, Clock, Settings } from 'lucide-angular';
+import { LucideAngularModule, Home, Calendar, BarChart3, Clock, Settings, Shield } from 'lucide-angular';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-bottom-nav',
@@ -17,4 +18,8 @@ export class BottomNavComponent {
   protected readonly BarChart3Icon = BarChart3;
   protected readonly ClockIcon = Clock;
   protected readonly SettingsIcon = Settings;
+  protected readonly ShieldIcon = Shield;
+
+  private userService = inject(UserService);
+  protected user = this.userService.getUser();
 }
